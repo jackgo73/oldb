1773918599c4b1f5478d3e6c8491c0eeec7b122f
JackGo <jackgo73@outlook.com>
2018-03-11 23:01:20 +0800

    Update tmp.cc

--- tmp.cc
+++ tmp.cc
@@ -1,80 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
 #include <cstdio>
 #include <iostream>
 #include <string> 
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <queue>
 
-
-//#define _DEBUG_VER
-/***********OJ_TEMPLATE_BY_MUTEX73***********/
-typedef int int32;
-typedef unsigned int uint32;
-typedef long long int64;
-typedef unsigned long long uint64;
-/***********SPECIFIC*************************/
-
-class Player {
-public:
-    Player(std::string _name, int32 _score) { 
-        name = _name; 
-        score = _score; 
-        score_history.push_back(std::pair<int32, int32>(0, score));
-    }
-
-    bool operator < (const Player& rhs) const {
-        if (score > rhs.score) {
-            return true;
-        } else if (score < rhs.score) {
-            return false;
-        } else {
-            int a = 0;
-            int b = 0;
-            while(score_history[a].second < score)
-                a++;
-            while(rhs.score_history[b].second < score)
-                b++;
-            return score_history[a].first < rhs.score_history[b].first;
-        }
-    }
-
-    std::string name;
-    int32 score;
-    std::vector<std::pair<int32, int32> > score_history;
-};
-
-
-int main()
-{
-#ifdef _DEBUG_VER
-    if(freopen("testcase.txt", "r", stdin) == NULL) {
-        fprintf(stderr,"error redirecting stdout\n");
-    }	
-#endif
-    int32 n, score, i, index;
-    std::vector<Player> players;
-    std::map<std::string, int32> players_index;
-    std::map<std::string, int32>::iterator iter;
-
-    std::string name;
-    std::ios::sync_with_stdio(false);
-    std::cin >> n;
-
-    index = 0;
-    for(i = 0; i < n; ++i) {
-        std::cin >> name >> score;
-        iter = players_index.find(name);
-        if (iter == players_index.end()) {
-            players_index.insert(std::map<std::string, int32>::value_type(name, index++));
-            players.push_back(Player(name, score));      
-        } else {
-            players[iter->second].score += score;
-            players[iter->second].score_history.push_back(std::pair<int32, int32>(i, players[iter->second].score));
-        }
-    }
-    sort(players.begin(), players.end());
-
-    std::cout << players[0].name << std::endl;
-
-    return 0;
+using namespace std;
+const int maxn = 1000011;
+int n, a[maxn], T[maxn];
+int main() {
+	if (freopen("testcase.txt", "r", stdin) == NULL) {
+		fprintf(stderr, "error redirecting stdout\n");
+	}
+	scanf("%d", &n);
+	static priority_queue<long long>q;
+	long long S = 0, ans;
+	for (int i = 1; i <= n; ++i) scanf("%d", a + i);
+	for (int i = 1; i <= n; ++i) scanf("%d", T + i);
+	for (int i = 1; i <= n; ++i) {
+		q.push(-(a[i] + S)); S += T[i];
+		ans = 0;
+		while (!q.empty() && -q.top() <= S) {
+			ans += -q.top() - S + T[i]; q.pop();
+		}
+		ans += q.size()*T[i];
+		printf("%lld ", ans);
+	}
+	int k;
 }
